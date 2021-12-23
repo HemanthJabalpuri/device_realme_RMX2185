a2d6455b811a1998e4606005cf726d6b12a6ce8f
phhusson <phhusson@users.noreply.github.com>
2021-12-23 17:45:52 +0700

    RMX2185: props: Disable debug.sf.latch_unsignaled [phhusson] - AOSP-12.0
    * On various Mali devices (MediaTek, Samsung), there are graphical glitches at many place.
      You can workaround this issue by setting debug.sf.latch_unsignaled property to 0
    
    Change-Id: If3fb9e02164148a6554fd2d7b47240ea7e2d9cad

--- system_prop.mk
+++ system_prop.mk
@@ -69,7 +69,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Surfaceflinger
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
