8f1f32596746ca41b4157cd05c86c8ccbf2ff858
Sarthak Roy <sarthakroy2002@gmail.com>
2021-12-26 10:11:57 +0700

    RMX2185: Inherit Ims blobs from external repo's

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ $(call inherit-product, vendor/realme/RMX2185/RMX2185-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# IMS
+$(call inherit-product, vendor/mtk-ims/ims.mk)
+
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # VNDK
